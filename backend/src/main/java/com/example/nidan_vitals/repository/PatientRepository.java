efabf9a84f3912086b67f803304b007fc422f35c
Ashutosh Paudyal <ashutoshpaudyal1@gmail.com>
2026-01-23 01:40:38 +0545

    feat: added functionality to savePatient

--- backend/src/main/java/com/example/nidan_vitals/repository/PatientRepository.java
+++ backend/src/main/java/com/example/nidan_vitals/repository/PatientRepository.java
@@ -3,7 +3,9 @@ package com.example.nidan_vitals.repository;
 import com.example.nidan_vitals.model.PatientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface PatientRepository extends JpaRepository<PatientEntity, Long> {
-
+    Optional<PatientEntity> findByPatientId(String patientId);
 }
