f0450a589105430062c176cbb7217ab6f0ab216f
Ashutosh Paudyal <ashutoshpaudyal1@gmail.com>
2026-01-23 01:42:21 +0545

    feat: added Patient Controller and functionality to createPatient

---
+++ backend/src/main/java/com/example/nidan_vitals/controller/PatientController.java
@@ -0,0 +1,27 @@
+package com.example.nidan_vitals.controller;
+
+import com.example.nidan_vitals.dtos.PatientRequestDTO;
+import com.example.nidan_vitals.dtos.ResponseMessage;
+import com.example.nidan_vitals.service.PatientService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequestMapping("/api/fhir")
+@RequiredArgsConstructor
+public class PatientController {
+
+    private final PatientService patientService;
+
+    @PostMapping("/patients")
+    public ResponseEntity<ResponseMessage> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
+        return new ResponseEntity<>(patientService.savePatient(patientRequestDTO), CREATED);
+    }
+
+}
