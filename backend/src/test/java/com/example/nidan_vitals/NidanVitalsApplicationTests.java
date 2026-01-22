2e427106b18afe2efa3c97b0f77d43d1cfb57964
Ashutosh Paudyal <ashutoshpaudyal1@gmail.com>
2026-01-23 00:45:16 +0545

    chore: initial project setup

---
+++ backend/src/test/java/com/example/nidan_vitals/NidanVitalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nidan_vitals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NidanVitalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
